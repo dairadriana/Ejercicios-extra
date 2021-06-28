517eb23c61594c31d08c0006130383bce7b3fe7a
Daira Rodríguez <A01274745@itesm.mx>
2021-06-28 09:28:24 -0500

    Otro método

--- num_dictionary.py
+++ num_dictionary.py
@@ -10,3 +10,19 @@ while True:
             print('Not found')
     except:
         break
+
+n = int(input())
+tel_dicc={}
+for _ in range(n):
+    name_telephone = input().split()
+    tel_dicc[name_telephone[0]] = name_telephone[1]
+
+while True:
+    try:
+        search = input()
+        if search in tel_dicc:
+            print("{}={}".format(search, tel_dicc[search]))
+        else:
+            print('Not found')
+    except:
+        break
